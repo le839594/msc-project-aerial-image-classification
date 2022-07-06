55ebbdee311eaecd15aad377957bf00fb6839170
le839594 <le839594@student.reading.ac.uk>
2022-07-06 11:09:32 +0100

    Updated code for first tuned model

--- cnn_tuned.py
+++ cnn_tuned.py
@@ -42,38 +42,39 @@ data_augmentation = tf.keras.Sequential([
 
 
 def build_model(hp):  # random search passes this hyperparameter() object
+    # define architecture layer by layer
     model = tf.keras.Sequential()
+    # add augmentation
     model.add(data_augmentation)
+    # apply normalisation
     model.add(tf.keras.layers.Rescaling(1. / 255))
-
+    # Convolutional layers and AveragePooling
     model.add(tf.keras.layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=(224, 224, 1)))
     model.add(tf.keras.layers.AveragePooling2D())
-
     model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))
     model.add(tf.keras.layers.AveragePooling2D())
-
     model.add(tf.keras.layers.Conv2D(filters=26, kernel_size=(3, 3), activation='relu'))
     model.add(tf.keras.layers.AveragePooling2D())
-
     model.add(tf.keras.layers.Conv2D(filters=36, kernel_size=(3, 3), activation='relu'))
     model.add(tf.keras.layers.AveragePooling2D())
-
     model.add(tf.keras.layers.Conv2D(filters=46, kernel_size=(3, 3), activation='relu'))
     model.add(tf.keras.layers.AveragePooling2D())
-
+    # flatten output before entering fully connected layers
     model.add(tf.keras.layers.Flatten())
-
+    # fully connected layers, the third is to be tuned
     model.add(tf.keras.layers.Dense(units=120, activation='relu'))
-
     model.add(tf.keras.layers.Dense(units=84, activation='relu'))
     units_3 = hp.Int('units_hp', min_value = 28, max_value = 84, step = 8)
-
     model.add(tf.keras.layers.Dense(units = units_3, activation='relu'))
+    # output layer that uses softmax to provide a classification
     model.add(tf.keras.layers.Dense(units=3, activation='softmax'))
+    # compile model
     model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
+# set batch size for tuning
 batch_size = 64
+# define Hyperband tuner used to tune the fully connected layer
 tuner = kt.Hyperband(build_model,
                      objective='val_accuracy',
                      max_epochs=50,
@@ -81,19 +82,18 @@ tuner = kt.Hyperband(build_model,
                      hyperband_iterations=3,
                      directory='tuning',
                      project_name='neuron tuning')
-
+# early stopping if no improvement in validation accuracy is shown after a set period
 stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=15)
-#mcp_save = ModelCheckpoint('.mdl_wts_v2.hdf5', save_best_only=True, monitor='val_accuracy', mode='max')
-
+# start search with Hyperband
 tuner.search(train_ds, validation_data = val_ds, batch_size=batch_size, epochs=150, callbacks=[stop_early])
-
-# Get the optimal hyperparameters
+# get the optimal hyperparameters identified
 best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
-
+# print the best number of neurons
 print(f"""
 The optimal number of units in the new densely-connected
 layer is {best_hps.get('units_hp')}
 """)
+
 
 # Build the model with the optimal hyperparameters and train it on the data for 50 epochs
 model = tuner.hypermodel.build(best_hps)
