97030464812bd224cdae1bb2668b295361d9c5bc
le839594 <le839594@student.reading.ac.uk>
2022-07-07 11:43:59 +0100

    Refactored previous code

--- image_viewer.py
+++ image_viewer.py
@@ -2,56 +2,62 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
-batch_size = 6
-img_height = 224
-img_width = 224
+
+def dataset_creation():
+  train_ds = tf.keras.utils.image_dataset_from_directory(
+    "C:/aerial_images",
+    validation_split=0.2,
+    subset="training",
+    label_mode="categorical",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+  val_ds = tf.keras.utils.image_dataset_from_directory(
+    "C:/aerial_images",
+    validation_split=0.2,
+    subset="validation",
+    label_mode="categorical",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+  return train_ds, val_ds
 
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  "C:/aerial_images",
-  validation_split=0.2,
-  subset="training",
-  label_mode="categorical",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+def augmentation_viewer(dataset, augmentations):
+  class_names = dataset.class_names
+  for images, labels in dataset.take(1):
+    for i in range(batch_size):
+      # plot original
+      plt.figure(figsize=(10, 10))
+      # ax = plt.subplot(6, 6, i + 1)
+      plt.imshow(images[i].numpy().astype("uint8"))
+      plt.title(class_names[np.argmax(labels[i])])
+      plt.axis("off")
+      plt.show()
+      # plot augmented
+      plt.figure(figsize=(10, 10))
+      augmented_image = augmentations(images[i])
+      plt.imshow(augmented_image.numpy().astype("uint8"))
+      plt.title(class_names[np.argmax(labels[i])])
+      plt.axis("off")
+      plt.show()
 
 
-val_ds =tf.keras.utils.image_dataset_from_directory(
-  "C:/aerial_images",
-  validation_split=0.2,
-  subset="validation",
-  label_mode="categorical",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+def create_augmentation():
+  all_augmentations = tf.keras.Sequential([
+  tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+  tf.keras.layers.RandomRotation(0.2)
+  ])
+
+  return all_augmentations
 
 
-
-class_names = train_ds.class_names
-
-all_augmentations = tf.keras.Sequential([
-    tf.keras.layers.RandomFlip("horizontal_and_vertical"),
-    tf.keras.layers.RandomRotation(0.2)
-])
-
-### To visualize the images
-
-for images, labels in train_ds.take(1):
-  for i in range(batch_size):
-    plt.figure(figsize=(10, 10))
-    #ax = plt.subplot(6, 6, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[np.argmax(labels[i])])
-    plt.axis("off")
-    plt.show()
-    plt.figure(figsize=(10, 10))
-    augmented_image = all_augmentations(images[i])
-    plt.imshow(augmented_image.numpy().astype("uint8"))
-    plt.title(class_names[np.argmax(labels[i])])
-    plt.axis("off")
-    plt.show()
-
-
-
-
+if __name__ == '__main__':
+  batch_size = 1
+  img_height = 224
+  img_width = 224
+  train_ds, val_ds = dataset_creation()
+  augmentations = create_augmentation()
+  augmentation_viewer(train_ds, augmentations)
